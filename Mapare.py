2b2e3e63d7bbbbb1f7a0bbc0edcf548e2f3a188e
catalinx132 <48054814+catalinx132@users.noreply.github.com>
2021-06-09 20:47:00 +0300

    Update Mapare.py

--- Mapare.py
+++ Mapare.py
@@ -1,3 +1,24 @@
+#vom mapa formatele de fișiere cu directorul
+
 FILE_FORMATS = {file_format: directory
                 for directory, file_formats in DIRECTORIES.items()
                 for file_format in file_formats}
+
+#Aici, mapăm extensiile de fișiere cu directorul.
+
+def organize_junk():
+    for entry in os.scandir():
+        if entry.is_dir():
+            continue
+        file_path = Path(entry)
+        file_format = file_path.suffix.lower()
+        if file_format in FILE_FORMATS:
+            directory_path = Path(FILE_FORMATS[file_format])
+            directory_path.mkdir(exist_ok=True)
+            file_path.rename(directory_path.joinpath(file_path))
+
+        for dir in os.scandir():
+            try:
+                os.rmdir(dir)
+            except:
+                pass
